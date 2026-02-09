fd7b29e311621c19ade7ee06774b28bed5a8395d
Harshmahajan29 <harshmahajan29125@gmail.com>
2026-02-10 00:55:40 +0530

    Changes done

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    const words = ["Student", "Learner", "Team Player", "Linux Enthusiast", "Developer", "Persistent", "Problem Solver", "Curious", "Creative Thinker", "Passionate Coder"];
+    const words = ["Student", "Learner", "Team Player", "Developer", "Persistent",  "Curious", "Creative", "Passionate", "Collaborative", "Adaptable", "Resilient", "Innovative", "Analytical", "Dedicated", "Motivated", "Enthusiastic", "Resourceful", "Proactive"];
     let index = 0;
 
     const textElement = document.getElementById("dynamic-text");
